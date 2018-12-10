28bd5cbc2991eb747c8b6e6bd025592e4246695c
Maltsev Nikita <mn121288@gmail.com>
2018-12-10 14:40:10 +0300

    Не выходит окошко с авторизацией и какие-то ошибки в консоли браузера

--- src/index.js
+++ src/index.js
@@ -2,22 +2,20 @@ import './style/index.css';
 import render from './templates/friends-content.hbs';
 import {dnd} from './js/dnd';
 
-
-const container = document.querySelector('.container');
-
 VK.init({
     apiId: 6769892
 });
 
-// const list = [
-//     {name: 'Иван', last_name: 'Иванов'},
-//     {name: 'Пётр', last_name: 'Петров'},
-//     {name: 'Максим', last_name: 'Максимов'},
-//     {name: 'Василий', last_name: 'Васильев'}
-//
-// ];
-//
-// // container.innerHTML = render( {items: list} );
-//
-// dnd();
+function auth() {
+    return new Promise((resolve, reject) => {
+        VK.auth.login(data => {
+            if (data.session) {
+                resolve();
+            } else {
+                reject(new Error('Не удалось авторизироваться'));
+            }
+        }, 2);
+    })
+}
+auth().then(() => console.log('ok'));
 
